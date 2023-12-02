7f221ec19b40af9e65c28ce44218c6513af14b6d
Kirill Arkhipenko <aks1983@mail.ru>
2023-12-02 21:44:13 +0100

    fixes

--- src/main.py
+++ src/main.py
@@ -1,40 +1,38 @@
-WORDS_TO_REPLACE = {
-    'one': '1',
-    'two': '2',
-    'three': '3',
-    'four': '4',
-    'five': '5',
-    'six': '6',
-    'seven': '7',
-    'eight': '8',
-    'nine': '9',
+WORD_TO_DIGIT = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
 }
-
-def main() -> None:
-    with open('01_input.txt', 'r') as file:
-        sum_result = sum(extract_digits(s[:-1]) for s in file)
-
-    print('Sum:', sum_result)
 
 
 def extract_digits(s: str) -> int:
-    r = ''
+    digits = [digit for i in range(0, len(s)) for word, digit in WORD_TO_DIGIT.items() if s[i:].startswith(word)]
 
-    for c in s:
-        r += c
+    result = 10 * digits[0] + digits[-1] if digits else None
 
-        for word, numeric_value in WORDS_TO_REPLACE.items():
-            if r.endswith(word):
-                r = r[:-len(word)] + numeric_value
+    return result
 
-    digits = [c for c in r if c.isdigit()]
 
-    if digits:
-        print(f'{s} -> {r} -> {digits[0]}{digits[-1]}')
-        return int(f'{digits[0]}{digits[-1]}')
+def main() -> None:
+    with open('01_input.txt') as file:
+        sum_result = sum(extract_digits(line) for line in file)
 
-    print(f'{s} -> {r} -> None')
-    return 0
+    print('Sum:', sum_result)
 
 
 if __name__ == '__main__':
--- src/main.rs
+++ src/main.rs
@@ -5,63 +5,59 @@ use std::io::{self, BufRead};
 use lazy_static::lazy_static;
 
 lazy_static! {
-    static ref WORDS_TO_REPLACE: HashMap<&'static str, char> = {
+    static ref WORD_TO_DIGIT: HashMap<&'static str, i32> = {
         let mut map = HashMap::new();
-        map.insert("one", '1');
-        map.insert("two", '2');
-        map.insert("three", '3');
-        map.insert("four", '4');
-        map.insert("five", '5');
-        map.insert("six", '6');
-        map.insert("seven", '7');
-        map.insert("eight", '8');
-        map.insert("nine", '9');
+        map.insert("1", 1);
+        map.insert("2", 2);
+        map.insert("3", 3);
+        map.insert("4", 4);
+        map.insert("5", 5);
+        map.insert("6", 6);
+        map.insert("7", 7);
+        map.insert("8", 8);
+        map.insert("9", 9);
+        map.insert("one", 1);
+        map.insert("two", 2);
+        map.insert("three", 3);
+        map.insert("four", 4);
+        map.insert("five", 5);
+        map.insert("six", 6);
+        map.insert("seven", 7);
+        map.insert("eight", 8);
+        map.insert("nine", 9);
         map
     };
 }
 
-fn main() -> io::Result<()> {
-    let file_path = "01_input.txt";
-    let file = File::open(file_path)?;
+fn extract_digits(s: &str) -> Option<i32> {
+    let mut digits = Vec::new();
 
-    let sum: u32 = io::BufReader::new(file)
-        .lines()
-        .filter_map(|line| line.ok().and_then(|s| extract_digits(&s)))
-        .map(|(first, last)| format!("{}{}", first, last).parse::<u32>().ok())
-        .filter_map(|parsed_number| parsed_number).sum();
-
-    println!("Sum: {}", sum);
-
-    Ok(())
-}
-
-fn extract_digits(s: &str) -> Option<(char, char)> {
-    print!("{}", s);
-
-    let mut r = String::new();
-
-    for c in s.chars() {
-        r.push(c);
-        for (word, &numeric_value) in WORDS_TO_REPLACE.iter() {
-            if r.ends_with(word) {
-                r.drain(r.len() - word.len()..);
-                r.push(numeric_value);
+    for i in 0..s.len() {
+        for (&word, &digit) in WORD_TO_DIGIT.iter() {
+            if s[i..].starts_with(word) {
+                digits.push(digit);
+                break;
             }
         }
     }
 
-    print!(" -> {}", r);
-
-    let digits: Vec<_> = r.chars().filter(|c| c.is_digit(10)).collect();
-
     if !digits.is_empty() {
-        let first = *digits.first().unwrap();
-        let last = *digits.last().unwrap();
-        println!(" -> {}{}", first, last);
-        return Some((first, last));
+        return Some(10 * digits.first().unwrap() + digits.last().unwrap());
     }
 
-    println!(" -> None");
+    return None;
+}
 
-    None
+fn main() {
+    if let Ok(file) = File::open("01_input.txt") {
+        let sum_result: i32 = io::BufReader::new(file)
+            .lines()
+            .filter_map(|line| line.ok())
+            .filter_map(|line| extract_digits(&line))
+            .sum();
+
+        println!("Sum: {}", sum_result);
+    } else {
+        eprintln!("Error reading the file.");
+    }
 }
