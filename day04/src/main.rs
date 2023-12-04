e5dfd1c38fb798db8593b472c8283d9a8ba0a7b6
Kirill Arkhipenko <aks1983@mail.ru>
2023-12-04 15:47:26 +0100

    refactoring

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -24,26 +24,24 @@ fn part1() {
 fn part2() {
     let content = fs::read_to_string("input.txt").expect("Failed to read file");
     let cards: Vec<&str> = content.lines().collect();
-    let mut cards_counter: HashMap<usize, usize> = HashMap::new();
 
-    for i in 0..cards.len() {
-        cards_counter.insert(i + 1, 1);
-    }
+    let mut cards_counter: HashMap<usize, usize> = (1..=cards.len()).map(|i| (i, 1)).collect();
 
     for card in cards {
         let mut card_parts = card.splitn(2, ':');
-        let card_info = card_parts.next().unwrap();
-        let numbers = card_parts.next().unwrap();
-        let card_number = card_info
+        let card_number = card_parts
+            .next()
+            .unwrap()
             .trim_start_matches("Card")
             .trim()
             .parse::<usize>()
             .expect("Failed to parse card number");
+        let numbers = card_parts.next().unwrap();
+        let numer_of_cards = cards_counter[&card_number];
         let mut numbers_parts = numbers.split('|');
         let win_numbers: Vec<&str> = numbers_parts.next().unwrap().split_whitespace().collect();
         let my_numbers: Vec<&str> = numbers_parts.next().unwrap().split_whitespace().collect();
         let number_of_matches = win_numbers.iter().filter(|x| my_numbers.contains(x)).count();
-        let numer_of_cards = cards_counter[&card_number];
 
         for i in 0..number_of_matches {
             *cards_counter.entry(card_number + i + 1).or_insert(0) += numer_of_cards;
