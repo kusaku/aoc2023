4f2a2caa38382a3090c1d6b1ae433b211e7dddfe
Kirill Arkhipenko <aks1983@mail.ru>
2023-12-08 02:01:20 +0100

    refactor day1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,11 +1,9 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{self, BufRead};
-
+use std::fs;
 use lazy_static::lazy_static;
 
 lazy_static! {
-    static ref WORD_TO_DIGIT1: HashMap<&'static str, i32> = [
+    static ref WORD_TO_DIGIT1: HashMap<&'static str, usize> = [
         ("1", 1),
         ("2", 2),
         ("3", 3),
@@ -18,10 +16,8 @@ lazy_static! {
     ]
     .into_iter()
     .collect();
-}
 
-lazy_static! {
-    static ref WORD_TO_DIGIT2: HashMap<&'static str, i32> = [
+    static ref WORD_TO_DIGIT2: HashMap<&'static str, usize> = [
         ("1", 1),
         ("2", 2),
         ("3", 3),
@@ -45,7 +41,7 @@ lazy_static! {
     .collect();
 }
 
-fn extract_digits(s: &str, word_to_digit: &HashMap<&'static str, i32>) -> Option<i32> {
+fn extract_digits(s: &str, word_to_digit: &HashMap<&str, usize>) -> Option<usize> {
     let mut digits = Vec::new();
 
     for i in 0..s.len() {
@@ -57,39 +53,31 @@ fn extract_digits(s: &str, word_to_digit: &HashMap<&'static str, i32>) -> Option
         }
     }
 
-    if !digits.is_empty() {
-        return Some(10 * digits.first().unwrap() + digits.last().unwrap());
+    if digits.is_empty() {
+        return None;
     }
 
-    return None;
+    return Some(10 * digits.first().unwrap() + digits.last().unwrap());
 }
 
 fn part1() {
-    if let Ok(file) = File::open("input.txt") {
-        let sum_result: i32 = io::BufReader::new(file)
-            .lines()
-            .filter_map(|line| line.ok())
-            .filter_map(|line| extract_digits(&line, &WORD_TO_DIGIT1))
-            .sum();
+    let input = fs::read_to_string("input.txt").expect("Failed to read file");
+    let r = input
+        .lines()
+        .filter_map(|line| extract_digits(line, &WORD_TO_DIGIT1))
+        .sum::<usize>();
 
-        println!("Answer: {}", sum_result);
-    } else {
-        eprintln!("Error reading the file.");
-    }
+    println!("Answer: {}", r);
 }
 
 fn part2() {
-    if let Ok(file) = File::open("input.txt") {
-        let sum_result: i32 = io::BufReader::new(file)
-            .lines()
-            .filter_map(|line| line.ok())
-            .filter_map(|line| extract_digits(&line, &WORD_TO_DIGIT2))
-            .sum();
+    let input = fs::read_to_string("input.txt").expect("Failed to read file");
+    let r = input
+        .lines()
+        .filter_map(|line| extract_digits(line, &WORD_TO_DIGIT2))
+        .sum::<usize>();
 
-        println!("Answer: {}", sum_result);
-    } else {
-        eprintln!("Error reading the file.");
-    }
+    println!("Answer: {}", r);
 }
 
 fn main() {
